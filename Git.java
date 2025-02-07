8b2d7e8ea80996b03f6ae333a9b754b045e54bc8
AdityaA135 <adityaagarwal664@gmail.com>
2025-02-07 10:54:11 +0530

    Assignment from 1st commit

---
+++ Git.java
@@ -0,0 +1,16 @@
+public class Git {
+    int n;
+    public Git(int n)
+    {
+        this.n=n;
+    }
+    public static void main(String[] args)
+    {
+        Git obj=new Git(5);
+        for(int i=0;i<obj.n;i++)
+        {
+            System.out.println("The numbers are: "+i+1);
+        }
+
+    }
+}
